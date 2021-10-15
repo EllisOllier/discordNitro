b1dd14b245d621490e0e4d8bfc701a3daf4cc85d
EllisOllier <ellisollier1@gmail.com>
2021-10-15 19:53:12 +0100

    Fixed discordNitroGen from instantly closing when all codes are printed out

--- discordNitroGen.py
+++ discordNitroGen.py
@@ -9,7 +9,12 @@ print("Developed by Fuze#8337")
 amountCodes = int(input("Please enter how many discord nitro codes you want to generate: "))
 
 for i in range(amountCodes):
+    amountCodes = amountCodes - 1
     code = "".join(random.choices(string.ascii_uppercase + string.digits + string.ascii_lowercase, k = 16))
     url = "https://discord.gift/" + code
     print(url)
     time.sleep(0.25)
+    if amountCodes == 0:
+        time.sleep(1)
+        print("Please close this window!")
+        time.sleep(9999)
